623719224bccc347c46815b44603ea9c2c280d78
rulep <rouan.ulep@gmail.com>
2021-09-08 23:02:25 -0800

    Updates

---
+++ precache-manifest.dcb5f18bf047fb7c22ef1eca6d826f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd01a1e3117c1e586deeefac936bbd49",
+    "url": "/Project-Pomodoro-Timer/index.html"
+  },
+  {
+    "revision": "27f399a8fa6b0eba7521",
+    "url": "/Project-Pomodoro-Timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/Project-Pomodoro-Timer/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Project-Pomodoro-Timer/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f399a8fa6b0eba7521",
+    "url": "/Project-Pomodoro-Timer/static/js/main.ecd14317.chunk.js"
+  },
+  {
+    "revision": "b381722be8b33e953ceb",
+    "url": "/Project-Pomodoro-Timer/static/js/runtime-main.f9727a46.js"
+  }
+]);
